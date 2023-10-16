cbff8e60d08639889a733810a180976be6a306cd
devsula50 <devsula50@gmail.com>
2023-10-16 11:13:09 +0900

    React Router with Data Loading (v6.4+)

--- 16-fast-react-pizza/src/ui/Header.jsx
+++ 16-fast-react-pizza/src/ui/Header.jsx
@@ -1,13 +1,16 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder.jsx";
 
 function Header() {
   return (
     <header>
-      <Link to='/'>Fast React Pizza Co.</Link>
+      <Link to="/">Fast React Pizza Co.</Link>
+
+      <SearchOrder />
 
       <p>Jonas</p>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
